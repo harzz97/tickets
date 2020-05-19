3ad07382dc7c15eb7e7f86609e8e95b88cc70584
Sriharish Balasubramanian <sriharish.b@redbus.com>
2020-05-19 14:54:06 +0530

    added env value for mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,14 +2,18 @@ import mongoose from 'mongoose'
 import { app } from './app'
 const PORT = 3000
 const start = async() => {
+    
+    if(!process.env.MONGO_URI){
+        throw new Error("AUTH MONGO URI not defined")
+    }
 
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex : true
         })
-        console.log("Connected to mongoose")
+        console.log("Auth connected to mongoose")
     } catch(err) {
         console.log("Mongo error",err)
     }
