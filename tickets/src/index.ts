d37abbad7cfd0c87fea5b1c497bf74e95d785ebe
Sriharish Balasubramanian <sriharish.b@redbus.com>
2020-05-27 16:24:37 +0530

    added listeners
    fixed some indentations

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { app } from './app'
 import { natsWrapper } from './nats-wrapper'
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener'
 const PORT = 3000
 const start = async () => {
 
@@ -18,18 +20,23 @@ const start = async () => {
     }
 
     try {
-        console.log(process.env.NATS_CLUSTER_ID,process.env.NATS_CLIENT_ID,process.env.NATS_URL)
+        console.log(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
         await natsWrapper.connect(
             process.env.NATS_CLUSTER_ID,
             process.env.NATS_CLIENT_ID,
             process.env.NATS_URL,
-            )
+        )
         natsWrapper.client.on('close', () => {
             console.log("NATS Connection closed")
             process.exit()
         })
         process.on('SIGINT', () => natsWrapper.client.close())
         process.on('SIGTERM', () => natsWrapper.client.close())
+
+        // setup the listeners
+        new OrderCreatedListener(natsWrapper.client).listen()
+        new OrderCancelledListener(natsWrapper.client).listen()
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -43,5 +50,5 @@ const start = async () => {
 }
 
 app.listen(PORT, () => { console.log(`Tickets Service listening on ${PORT} `) })
-  
+
 start()
