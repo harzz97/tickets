a1f721e4a521c6729ce4f7a81474f49cd8f80553
Sriharish Balasubramanian <sriharish.b@redbus.com>
2020-05-27 18:12:09 +0530

    added listener

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from './nats-wrapper'
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
 const start = async () => {
     console.log("Expiration service started")
     if (!process.env.NATS_CLIENT_ID) {
@@ -25,6 +26,8 @@ const start = async () => {
         process.on('SIGINT', () => natsWrapper.client.close())
         process.on('SIGTERM', () => natsWrapper.client.close())
 
+        // add the ordercreated listener
+        new OrderCreatedListener(natsWrapper.client).listen()
     } catch (err) {
         console.log("Mongo error", err)
     }
