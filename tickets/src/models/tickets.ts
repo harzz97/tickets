c81204801d7db3b9e77faf766b885349caf52d33
Sriharish Balasubramanian <sriharish.b@redbus.com>
2020-05-27 14:51:05 +0530

    added version and orderId

--- tickets/src/models/tickets.ts
+++ tickets/src/models/tickets.ts
@@ -11,7 +11,8 @@ interface TicketDoc extends mongoose.Document {
     title: string,
     price: number,
     userId: string,
-    version: number
+    version: number,
+    orderId?: string
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -30,6 +31,9 @@ const ticketSchema = new mongoose.Schema({
     userId: {
         type: String,
         required: true
+    },
+    orderId: {
+        type: String
     }
 }, {
     toJSON: {
@@ -39,7 +43,7 @@ const ticketSchema = new mongoose.Schema({
         }
     }
 })
-ticketSchema.set('versionKey','version')
+ticketSchema.set('versionKey', 'version')
 ticketSchema.plugin(updateIfCurrentPlugin)
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs)
