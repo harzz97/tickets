f7d4ab053adc4fdd52900400113e06f8324b07bf
Sriharish Balasubramanian <sriharish.b@redbus.com>
2020-05-27 12:08:56 +0530

    added version control for tickets

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,15 +1,18 @@
 import mongoose from 'mongoose'
-import { Order, OrderStatus } from './orders'; 
-
+import { Order, OrderStatus } from './orders';
+import 'mongoose-update-if-current'
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 interface TicketAttrs {
     title: string,
-    price: number
+    price: number,
+    id: string
 }
 
 export interface TicketDoc extends mongoose.Document {
     title: string,
     price: number,
-    isReserved() : Promise<Boolean>
+    version: number;
+    isReserved(): Promise<Boolean>
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -35,11 +38,18 @@ const ticketSchema = new mongoose.Schema({
     }
 });
 
+ticketSchema.set('versionKey','version')
+ticketSchema.plugin(updateIfCurrentPlugin)
+
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-    return new Ticket(attrs)
+    return new Ticket({
+        _id: attrs.id,
+        title: attrs.title,
+        price: attrs.price
+    })
 }
 
-ticketSchema.methods.isReserved = async function(){
+ticketSchema.methods.isReserved = async function () {
     // this === the document that waas used to call this function
     const isExistingOrder = await Order.findOne({
         ticket: this,
